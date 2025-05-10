f4665d4a57c145282b3626e739e64f2b79679aac
yugeng58 <124924163+yugeng58@users.noreply.github.com>
2025-05-11 00:42:48 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "numc.cpp"
+using namespace std;
+
+int main() {
+
+    int shape[1] = {10};
+    numc<int> A = numc<int>(1,shape);
+    numc<int> B = numc<int>(1,shape);
+    numc<int> C = numc<int>(1,shape);
+    numc<int> F = numc<int>(1,shape);
+
+    for(int i = 0;i < 10;i++){
+        A[i].get() = i + 1;
+        B[i].get() = i + 1;
+        C[i].get() = i + 1;
+    }
+
+
+    A.reshape(3,new int[3]{10,1,1});
+    B.reshape(3,new int[3]{1,10,1});
+    C.reshape(3,new int[3]{1,1,10});
+    numc<int> D = A*B*C;
+
+    D.print();
+
+    D.slice(0,5,1).print();
+
+    return 0;
+}
